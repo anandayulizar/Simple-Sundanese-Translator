8f78936ed76553e08c964b81b7f948b2101470c8
anandayulizar <anandayulizar@gmail.com>
2020-05-22 01:03:00 +0700

    add styling

--- src/App.py
+++ src/App.py
@@ -1,15 +1,5 @@
-from flask import Flask, escape, render_template, url_for, redirect, request
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField, TextField
-from wtforms.validators import DataRequired
+from flask import Flask, render_template, url_for,request
 from Translator import translate
-
-class translationForm(FlaskForm):
-    left = TextField('Indonesia', validators=[DataRequired()])
-    right = TextField('Sunda', validators=[DataRequired()])
-    pattern = RadioField('Choose Your Pattern', choices=[('KMP', 'KMP'), ('BM', 'BM'), ('Regex', 'Regex')], validators=[DataRequired()])
-
-    submit = SubmitField('Match Key!')
 
 app = Flask(__name__)
 
@@ -18,13 +8,10 @@ def index():
     try:
         sentence = request.form['left-translation']
         matcher = request.form['algorithm']
-        language = request.form['submit'].lower() + '.txt'
-        if (request.form['submit'] == 'Sunda'):
-            isSunda = True
-        else:
-            isSunda = False
+        dictionary = request.form['submit'].lower() + '.txt'
+        isSunda = request.form['submit'] == 'Sunda'
 
-        result, isSentence = translate(language, sentence, matcher)
+        result, isSentence = translate(dictionary, sentence, matcher)
 
         return render_template('index.html', sentence=sentence, matcher=matcher, result=result, isSentence=isSentence, isSunda=isSunda)   
     except:   
